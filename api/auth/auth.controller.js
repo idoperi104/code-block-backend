93268e821f279bbe1c8d071001dfa1916c6acca7
idoperi104 <idoperi104@gmail.com>
2023-11-23 19:09:31 +0200

    set atlas mongoDb

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -19,17 +19,18 @@ async function login(req, res) {
 async function signup(req, res) {
   try {
     const credentials = req.body
-    // Never log passwords
-    // logger.debug(credentials)
     const account = await authService.signup(credentials)
+    
+    // Never log passwords
     logger.debug(`auth.route - new account created: ` + JSON.stringify(account))
+
     const user = await authService.login(
       credentials.username,
       credentials.password
     )
+
     logger.info("User signup:", user)
     const loginToken = authService.getLoginToken(user)
-    // res.cookie("loginToken", loginToken, { sameSite: "None", secure: true })
     res.cookie("loginToken", loginToken)
     res.json(user)
   } catch (err) {
